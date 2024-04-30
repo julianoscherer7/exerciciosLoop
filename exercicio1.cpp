19b9b06118b745306eb110726671c721af12e632
JULIANO MARTINS SCHERER <04471070037@senacrs.edu.br>
2024-04-29 21:43:53 -0300

    a

---
+++ exercicio1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    using namespace std;
+int array[]= {1, 2, 3, 4, 5};
+for (size_t i = 0; i < 5; ++i) {
+    cout << array[i] << " ";
+}
+cout << endl;
+return 0;
+
+}
---
+++ exercicio2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char letras[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int i = 0;
+    while (i < 6) {
+        cout << letras[i] << endl;
+        i++;
+    }
+    return 0;
+}
